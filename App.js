8368c872ebb259b5b14f1ab8c79873f6dbcc4edc
anantvsingh <ianantvsingh@gmail.com>
2023-10-02 11:48:23 +0530

    Code Added to Appjs

--- App.js
+++ App.js
@@ -1,1 +1,18 @@
 // app.js
+//Code Added to Appjs
+const { View, Text, StyleSheet } = require("react-native");
+
+function App(){
+  return (<View style={styles.container}>
+    <Text>hello bambu</Text>
+  </View>)
+}
+export default App;
+
+const styles=StyleSheet.create({
+  container:{
+    color:'black',
+    alignItems:"center"
+
+  }
+})
