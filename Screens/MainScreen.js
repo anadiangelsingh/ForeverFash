4900afd9a94d8d67f987422fe78ca75b4484a5b0
anadiangelsingh <anadiangelsingh@gmail.com>
2023-10-04 21:40:38 +0530

    Code update

--- Screens/MainScreen.js
+++ Screens/MainScreen.js
@@ -1,20 +1,21 @@
-import { Text, View,StyleSheet } from "react-native";
-import Header from "../Components/Header";
-import SubHeader from "../Components/SubHeader";
+import {Text, View, StyleSheet} from 'react-native';
+import Header from '../Components/Header';
+import SubHeader from '../Components/SubHeader';
 
-function MainScreen(){
-    return (
-        <View ><Header/>
-        <Text>hello</Text>
-        <SubHeader/></View>
-    )
+function MainScreen() {
+  return (
+    <View>
+      <Header />
+      <SubHeader />
+    </View>
+  );
 }
 
 export default MainScreen;
 
-const styles=StyleSheet.create({
-    container:{
-        flexDirection:"column",
-        flex:1
-    }
-})
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    flex: 1,
+  },
+});
